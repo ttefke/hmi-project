ef8a8b7696291a087a95236e13fb9f0d0bc4d2a2
Tobias Tefke <tobias@tefke.name>
2024-05-16 16:52:30 +0200

    Containerize

---
+++ test.sh
@@ -0,0 +1,6 @@
+curl --request GET \
+--url 'http://127.0.0.1/llm/vectorise/?=' \
+--header 'Content-Type: application/json' \
+--data '{
+"language": "en",
+"vectorise": ["Identify relevant market framework conditions of carmakers\n Realize the economic importance of the automotive industry\n Know automotive key figures in respect to particular countries\n Categorize the product portfolio of automotive suppliers\n Examine and understand future trends of automotive markets and resource requirements of carmakers"]}'
